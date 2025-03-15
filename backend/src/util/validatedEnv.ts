9a233b5619bde682afcc19d32ecba15a3851a3ee
anir183 <188557785+anir183@users.noreply.github.com>
2025-03-16 01:36:59 +0530

    feat: update env var validator

--- backend/src/util/validatedEnv.ts
+++ backend/src/util/validatedEnv.ts
@@ -1,8 +1,10 @@
 import "@dotenvx/dotenvx/config";
-import { bool, cleanEnv, port, str } from "envalid";
+import { bool, cleanEnv, port, str, num } from "envalid";
 
 export default cleanEnv(process.env, {
     MONGO_CONNECTION_STR: str(),
     PORT: port(),
-    LOG: bool()
+    LOG: bool(),
+    SESSION_SECRET: str(),
+    HASH_NUM: num()
 });
