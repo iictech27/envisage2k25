ef938d50d8021333ac259a3d7f17454be3b8446b
anir183 <188557785+anir183@users.noreply.github.com>
2025-03-16 01:43:22 +0530

    feat: update logging and errors
    fix: update loggin and errors

--- backend/src/server/controllers/root.ts
+++ backend/src/server/controllers/root.ts
@@ -1,10 +1,12 @@
 import { RequestHandler } from "express";
+import createHttpError from "http-errors";
+import httpCodes from "../../util/httpCodes.js";
 
 // root "/" get endpoint
 export const getServerStatus: RequestHandler = (_req, res, next) => {
     try {
         res.send("Server is Running!");
     } catch(error) {
-        next(error);
+        next(createHttpError(httpCodes["503"].code, httpCodes["503"].message + ": Server Down!"));
     }
 };
--- backend/src/server/server.ts
+++ backend/src/server/server.ts
@@ -2,9 +2,11 @@ import express, { Request, Response, NextFunction } from "express";
 
 import validatedEnv from "../util/validatedEnv.js";
 import { log, logErr } from "../util/logger.js";
+import { log, logErr, startHttpReqLogging } from "../util/logger.js";
 import usersRouter from "./routes/users.js";
 import rootRouter from "./routes/root.js";
 import httpCodes from "../util/httpCodes.js";
+import createHttpError, { isHttpError } from "http-errors";
 
 const server = express();
 const port = validatedEnv.PORT;
@@ -16,10 +18,16 @@ function startServer() : void {
         log("Listening at port " + port);
     });
 
+    // log http requests
+    startHttpReqLogging(server);
 
     // parse json body
     server.use(express.json());
 
+    // catch favicon.ico requests which are sent by some browsers
+    server.get("/favicon.ico", (_req: Request, res: Response) => {
+        res.status(httpCodes["204"].code);
+    });
 
     // different routes/endpoints
     server.use("/", rootRouter);
@@ -29,6 +37,7 @@ function startServer() : void {
     // non-existent endpoint handler
     server.use((_req: Request, _res: Response, next: NextFunction) => {
         next(Error("Endpoint not found."));
+        next(createHttpError(httpCodes["404"].code, httpCodes["404"].message + ": Endpoint not found."));
     });
 
 
@@ -36,6 +45,12 @@ function startServer() : void {
     // error handling
     server.use((error: unknown, _req: Request, res: Response, _next: NextFunction) => {
         logErr(error, "server.ts");
+
+        if(isHttpError(error)) {
+            res.status(error.statusCode);
+            res.send(error.message);
+            return;
+        }
         res.status(httpCodes["500"].code);
         res.send(error instanceof Error ? error.message : "Unknown Error");
     });
