e43a4470c8cf019eaac8af997e625ca786b566b6
anir183 <188557785+anir183@users.noreply.github.com>
2025-03-19 22:10:30 +0530

    fix: wrong import and request type

--- backend/src/server/routes/registration.ts
+++ backend/src/server/routes/registration.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
 
-import { createRegistration, getUserRegistrations } from "../controllers/registration.js";
+import { createRegistration } from "../controllers/registration.js";
 import { requireAuthUser } from "../middleware/user.js";
 
 const registrationRouter = Router();
 
-registrationRouter.get("/reg/new", requireAuthUser, createRegistration); // new registration
+registrationRouter.post("/reg/new", requireAuthUser, createRegistration); // new registration
 
 export default registrationRouter;
