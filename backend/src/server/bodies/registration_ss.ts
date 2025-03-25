8d94fc2c3b7cfd2d03be70970a01aac25a56f4f9
Anirban Dutta <meanirbandutta22@gmail.com>
2025-03-25 10:50:05 +0530

    fix - backend issues

--- backend/src/server/bodies/registration_ss.ts
+++ backend/src/server/bodies/registration_ss.ts
@@ -1,26 +1,26 @@
 // request bodies
 
 export interface ReqSSRegistrationOrderBody {
-    fullName?: string,
-    email?: string,
-    year?: number,
-    phone?: string,
-    college?: string,
-    eventIDs?: number[],
-    additionalInfo?: string
+  fullname?: string;
+  email?: string;
+  year?: number;
+  phone?: string;
+  college?: string;
+  eventIDs?: number[];
+  additionalInfo?: string;
 }
 
 // response bodies
 
 export interface ResSSRegistrationBody {
-    status: number,
-    message: string,
-    userFullName: string,
-    userYear: number,
-    userEmail: string,
-    userPhone: string,
-    userCollege: string,
-    newRegisteredEventIDs: number[],
-    price: number,
-    details: string
+  status: number;
+  message: string;
+  userFullName: string;
+  userYear: number;
+  userEmail: string;
+  userPhone: string;
+  userCollege: string;
+  newRegisteredEventIDs: number[];
+  price: number;
+  details: string;
 }
